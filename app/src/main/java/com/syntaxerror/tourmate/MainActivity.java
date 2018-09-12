2f205154f501fdf53bcab697d7c53e66dae5ae93
nadim298 <298nadim@gmail.com>
2018-09-12 15:37:55 +0600

    toast added

--- app/src/main/java/com/syntaxerror/tourmate/MainActivity.java
+++ app/src/main/java/com/syntaxerror/tourmate/MainActivity.java
@@ -2,12 +2,16 @@ package com.syntaxerror.tourmate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(),"TourMate prpoject opened successfully",Toast.LENGTH_SHORT).show();
     }
 }
